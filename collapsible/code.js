f053a766defd95e1017beb15794987682e45a322
Geoffrey Buell <info@geoffreybuell.com>
2021-03-09 11:54:39 -0500

    Created and styled collapsible

---
+++ collapsible/code.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const btn = document.querySelector('.collapse-btn');
+  const icon = document.querySelector('.icon');
+  const content = document.querySelector('.content');
+  let isOpen = true;
+
+  // toggle fn
+
+  btn.addEventListener('click', () => {
+    // toggle
+  });
+
+});
