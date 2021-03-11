369030062e093d3f207e7c7e5bc2405e30ab2f6f
Geoffrey Buell <info@geoffreybuell.com>
2021-03-11 14:49:10 -0500

    Finished writing search-bar

--- search-bar/code.js
+++ search-bar/code.js
@@ -1,45 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const productSearch = document.getElementById('product-search');
+  const clearBtn = document.querySelector('.fa-times');
+  const results = document.querySelector('.results-wrapper');
+  
   let products = [
-    'Apples', 'Bananas', 'Grapefruit', 'Kiwi', 'Avocados', 
-    'Lettuce', 'Tomatoes', 'Cheese', 'Bread', 'Yogurt', 
-    'Peas', 'Carrots', 'Broccoli', 'Beans', 'Pizza'
+    'apples', 'bananas', 'grapefruit', 'kiwi', 'avocados', 
+    'lettuce', 'tomatoes', 'cheese', 'bread', 'yogurt', 
+    'peas', 'carrots', 'broccoli', 'beans', 'pizza',
+    'pasta', 'rice', 'cereal', 'butter', 'milk',
+    'eggs', 'onions', 'garlic', 'honey', 'soup',
+    'salt', 'pepper', 'oregano', 'basil', 'paprika'
   ];
 
-  const productSearch = document.getElementById('product-search');
-  productSearch.addEventListener('keyup', (e) => {
-    // filter
+  const list = document.createElement('ul');
+  list.className = 'list';
+
+  products.forEach((product) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.textContent = product;
+    a.href = '#';
+    li.appendChild(a);
+    li.className = 'list-item';
+    list.appendChild(li);
+  });
+  
+  results.appendChild(list);
+
+  const displayFiltered = () => {
+    const filter = productSearch.value.toLowerCase();
+    const items = list.getElementsByTagName('li');
+    for (let i = 0; i < items.length; i++) {
+      const a = items[i].getElementsByTagName('a')[0];
+      const txtVal = a.textContent || a.textContent;
+      if (txtVal.toLowerCase().indexOf(filter) > -1) {
+        items[i].style.display = '';
+      } else {
+        items[i].style.display = 'none';
+      }
+    }
+  }  
+  
+  productSearch.addEventListener('keyup', () => {
+    displayFiltered();
   });
 
-  const clearBtn = document.querySelector('.fa-times');
   clearBtn.addEventListener('click', () => {
     productSearch.value = '';
+    displayFiltered();
   });
 
-  const displayProducts = () => {
-    const list = document.createElement('ul');
-
-    products.forEach((product) => {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      a.textContent = product;
-      a.href = '#';
-      li.appendChild(a);
-      li.className = 'list-item';
-      list.appendChild(li);
-    });
-
-    const results = document.querySelector('.results-wrapper');
-    results.appendChild(list);
-  }
-
-  displayProducts();
-
-  // const filterProducts = () => {
-  //   const filteredProducts = products.filter(product => product.length > 6);
-  //   console.log(filteredProducts);
-  // }
-
-  // filterProducts();
-  
 });
