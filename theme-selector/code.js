054cc680c4ae842d34a683e1ecb91862ef4264dc
Geoffrey Buell <info@geoffreybuell.com>
2021-03-17 11:02:27 -0400

    Got all the themes set up

--- theme-selector/code.js
+++ theme-selector/code.js
@@ -15,11 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
     almostBlack: '#0B090A'
   }
 
-  // =============== FILLER INFO =============== //
+  // =============== EXAMPLE ARTICLE INFO =============== //
 
   let headline = 'Hello World!';
 
-  let subheadline = 'You can update your display preferences. Cool beans!';
+  let subheadline = 'You can update your display preferences.';
 
   let author = 'This Guy';
 
@@ -33,10 +33,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const container = document.createElement('div');
   container.className = 'container';
-  container.style.backgroundColor = colors.lightGray;
 
   const header = document.createElement('header');
-  header.style.backgroundColor = colors.warmGray;
 
   const headerContent = document.createElement('div');
   headerContent.className = 'header-content';
@@ -44,62 +42,107 @@ document.addEventListener('DOMContentLoaded', () => {
   const logo = document.createElement('div');
   logo.className = 'logo';
   logo.textContent = 'LOGO';
-  logo.style.color = colors.almostBlack;
-  logo.style.border = `0.2rem solid ${colors.almostBlack}`;
+  logo.style.color = colors.tomatoRed;
+  logo.style.border = `0.2rem solid ${colors.tomatoRed}`;
 
-  const pref = document.createElement('div');
-  pref.className = 'pref';
-  pref.style.border = `0.1rem solid ${colors.gray}`;
+  const dropdown = document.createElement('div');
+  dropdown.className = 'dropdown';
 
-  const lightBtn = document.createElement('button');
-  lightBtn.textContent = 'Light';
-  lightBtn.style.color = colors.white;
-  lightBtn.style.border = `0.1rem solid ${colors.gray}`;
+  const dropBtn = document.createElement('button');
+  dropBtn.className = 'drop-btn';
+  dropBtn.textContent = 'Preferences';
+
+  const dropContent = document.createElement('div');
+  dropContent.className = 'drop-content';
+  dropContent.style.backgroundColor = colors.lightGray;
+  dropContent.style.border = `0.1rem solid ${colors.darkGray}`;
   
-  const darkBtn = document.createElement('button');
-  darkBtn.textContent = 'Dark';
-  darkBtn.style.color = colors.white;
-  darkBtn.style.border = `0.1rem solid ${colors.gray}`;
+  const light = document.createElement('a');
+  light.textContent = 'Light Mode';
+  light.href = '#';
+  
+  const dark = document.createElement('a');
+  dark.textContent = 'Dark Mode';
+  dark.href = '#';
 
-  const redBtn = document.createElement('button');
-  redBtn.textContent = 'Red';
-  redBtn.style.color = colors.white;
-  redBtn.style.border = `0.1rem solid ${colors.gray}`;
+  const red = document.createElement('a');
+  red.textContent = 'Red Mode';
+  red.href = '#';
 
   const main = document.createElement('main');
-  main.style.backgroundColor = colors.white;
 
   const h1 = document.createElement('h1');
   h1.textContent = headline;
-  h1.style.color = colors.darkGray;
   h1.style.marginTop = '3rem';
 
   const h3 = document.createElement('h3');
   h3.textContent = subheadline;
-  h3.style.color = colors.darkGray;
 
   const details = document.createElement('p');
   details.textContent = `${publishDate} | Written by ${author}`;
   details.style.fontSize = '0.9rem';
-  details.style.color = colors.warmGray;
 
   const p = document.createElement('p');
   p.textContent = bodyText;
-  p.style.color = colors.darkGray;
   p.style.lineHeight = '1.6rem';
+
+  // =============== Functions =============== //
+
+  function applyLight() {
+    container.style.backgroundColor = colors.lightGray;
+    header.style.backgroundColor = colors.lightGray;
+    dropBtn.style.color = colors.darkGray;
+    dropBtn.style.border = `0.1rem solid ${colors.darkGray}`;
+    main.style.backgroundColor = colors.white;
+    h1.style.color = colors.darkGray;
+    h3.style.color = colors.darkGray;
+    details.style.color = colors.warmGray;
+    p.style.color = colors.darkGray;
+  }
+
+  function applyDark() {
+    container.style.backgroundColor = colors.almostBlack;
+    header.style.backgroundColor = colors.almostBlack;
+    dropBtn.style.color = colors.lightGray;
+    dropBtn.style.border = `0.1rem solid ${colors.lightGray}`;
+    main.style.backgroundColor = colors.darkGray;
+    h1.style.color = colors.gray;
+    h3.style.color = colors.gray;
+    details.style.color = colors.warmGray;
+    p.style.color = colors.gray;
+  }
+
+  function applyRed() {
+    container.style.backgroundColor = colors.darkRed;
+    header.style.backgroundColor = colors.darkRed;
+    dropBtn.style.color = colors.tomatoRed;
+    dropBtn.style.border = `0.1rem solid ${colors.tomatoRed}`;
+    main.style.backgroundColor = colors.red;
+    h1.style.color = colors.darkGray;
+    h3.style.color = colors.darkGray;
+    details.style.color = colors.darkGray;
+    p.style.color = colors.almostBlack;
+  }
+
+  function addContent() {
+    body.appendChild(container);
+    container.appendChild(header);
+    container.appendChild(main);
+    header.appendChild(headerContent);
+    headerContent.appendChild(logo);
+    headerContent.appendChild(dropdown);
+    dropdown.appendChild(dropBtn);
+    dropdown.appendChild(dropContent);
+    dropContent.appendChild(light);
+    dropContent.appendChild(dark);
+    dropContent.appendChild(red);
+    main.appendChild(h1);
+    main.appendChild(h3);
+    main.appendChild(details);
+    main.appendChild(p);
+  }
   
-  body.appendChild(container);
-  container.appendChild(header);
-  container.appendChild(main);
-  header.appendChild(headerContent);
-  headerContent.appendChild(logo);
-  headerContent.appendChild(pref);
-  pref.appendChild(lightBtn);
-  pref.appendChild(darkBtn);
-  pref.appendChild(redBtn);
-  main.appendChild(h1);
-  main.appendChild(h3);
-  main.appendChild(details);
-  main.appendChild(p);
+  addContent();
+  applyDark();
 
 });
