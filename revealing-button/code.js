32cdcaa5ede268408c6b2f2567199d5baaa20e43
Geoffrey Buell <info@geoffreybuell.com>
2021-03-17 16:56:35 -0400

    Stylized hidden gem, button is sort of there but wired up

---
+++ revealing-button/code.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const btn = document.querySelector('.btn');
+  btn.addEventListener('click', () => {
+    btn.style.display = 'none';
+  });
+
+});
