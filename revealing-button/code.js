52752bec9de1559a87f2b309dd27151399012573
Geoffrey Buell <info@geoffreybuell.com>
2021-03-18 15:37:39 -0400

    Changed the concept of the project and created a ruby in a glass case

--- revealing-button/code.js
+++ revealing-button/code.js
@@ -1,8 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const btn = document.querySelector('.btn');
-  btn.addEventListener('click', () => {
-    btn.style.display = 'none';
-  });
+  // Ruby divs
+  const rubyWrap = document.querySelector('.ruby-wrap');
+  const upperLeft = document.querySelector('.r-upper-left');
+  const upperMiddle = document.querySelector('.r-upper-middle');
+  const upperRight = document.querySelector('.r-upper-right');
+
+  // Ruby Colors
+  // Colors start with red, light red, highlight red, and then shadow red.
+  const rubyColors = ['#bd2222', '#de4040', '#fa6161', '#a31414'];
+
+  const color0 = `40px solid ${rubyColors[0]}`,
+        color1 = `40px solid ${rubyColors[1]}`,
+        color2 = `40px solid ${rubyColors[2]}`;
+
+  // Ruby Rotation Fns
+  function changeColors() {
+    upperLeft.style.borderBottom = color0;
+    upperMiddle.style.borderTop = color1;
+    upperRight.style.borderBottom = color2;
+    setTimeout(() => {
+      upperLeft.style.borderBottom = color2;
+      upperMiddle.style.borderTop = color1;
+      upperRight.style.borderBottom = color0;
+      }, 500);
+  }
+
+  function rotateRuby() {
+    setInterval(() => {
+      changeColors();
+    }, 1000);
+  }
+
+  rotateRuby();
+
 
 });
