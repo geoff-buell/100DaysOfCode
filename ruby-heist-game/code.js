5c8ca8e1b350c5bec4e4224c3883f81eb4b60586
Geoffrey Buell <info@geoffreybuell.com>
2021-03-19 08:50:08 -0400

    Scrapped the glass box idea, moving forward with alarm maze

--- ruby-heist-game/code.js
+++ ruby-heist-game/code.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   // Ruby divs
-  const rubyWrap = document.querySelector('.ruby-wrap');
+  const ruby = document.querySelector('.ruby-wrap');
   const upperLeft = document.querySelector('.r-upper-left');
   const upperMiddle = document.querySelector('.r-upper-middle');
   const upperRight = document.querySelector('.r-upper-right');
@@ -13,6 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const color0 = `40px solid ${rubyColors[0]}`,
         color1 = `40px solid ${rubyColors[1]}`,
         color2 = `40px solid ${rubyColors[2]}`;
+
+  
+  const alarmBox = document.querySelector('.alarm-box');
 
   // Ruby Rotation Fns
   function changeColors() {
@@ -34,5 +37,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
   rotateRuby();
 
+  // dragRuby(ruby);
+
+  function dragRuby(elmnt) {
+    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.querySelector('.glass')) {
+      document.querySelector('.glass').onmousedown = dragMouseDown();
+    } else {
+      elmnt.onmousedown = dragMouseDown();
+    }
+
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
 
 });
