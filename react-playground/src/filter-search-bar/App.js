1db5d17398bb16f1ef8e767761049a9eda5765c1
Geoffrey Buell <info@geoffreybuell.com>
2021-03-30 16:34:48 -0400

    Added filter-search-bar to react playground

---
+++ react-playground/src/filter-search-bar/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import './filter-search-bar.css';
+
+function App() {
+  let products = [
+    'apples', 'bananas', 'grapefruit', 'kiwi', 'avocados', 
+    'lettuce', 'tomatoes', 'cheese', 'bread', 'yogurt', 
+    'peas', 'carrots', 'broccoli', 'beans', 'pizza',
+    'pasta', 'rice', 'cereal', 'butter', 'milk',
+    'eggs', 'onions', 'garlic', 'honey', 'soup',
+    'salt', 'pepper', 'oregano', 'basil', 'paprika'
+  ];
+  
+  return (
+    <div>
+      <SearchBar products={products} />
+    </div>
+  );
+}
+
+export default App;
