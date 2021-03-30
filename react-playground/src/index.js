c4f05c5323654326aaada7ddabbed18c46813775
Geoffrey Buell <info@geoffreybuell.com>
2021-03-30 16:36:54 -0400

    Updated app version in index.js

--- react-playground/src/index.js
+++ react-playground/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import App from './header-and-navbar/App';
-import App from './counter-buttons/App';
+// import App from './counter-buttons/App';
+// import App from './word-counter/App';
+import App from './filter-search-bar/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
