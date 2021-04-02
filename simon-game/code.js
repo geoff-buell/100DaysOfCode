03ddf7c32f5c5a0737ac243599488d0545e5ef77
Geoffrey Buell <info@geoffreybuell.com>
2021-04-02 15:52:30 -0400

    Wrote out requirements for the simon game js

---
+++ simon-game/code.js
@@ -0,0 +1,13 @@
+// REQUIREMENTS
+// ------------------ //
+// 0) Game must turn on. Power switch floats right. Count lights up.
+// 1) If pressed, strict btn must indicate on and allow no faults from player.
+// 2) Once pressed, start btn begins game.
+// ------------------ //
+// 3) Game should have a different sound associated with each btn-main.
+// 4) Game should present random series of btn presses.
+// 5) Game should increment by one btn press every time player is correct.
+// 6) Game should keep track of series and be able to recognize player input to match.
+// 7) Game should indicate when player is wrong. Strict mode starts over after this.
+// 8) Game should show count. Pace should increase every increment of 5.
+// 9) Game should indicate win if player reaches 20.
