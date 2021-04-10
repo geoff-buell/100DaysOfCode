f86e9e31b9834bf1e9973bb240868261f22b5382
Geoffrey Buell <info@geoffreybuell.com>
2021-04-10 15:36:45 -0400

    Added mobile scalability, triangle keyframes, fadeIn fn, fadeOut fn, and shake fn

--- magic-8-ball/code.js
+++ magic-8-ball/code.js
@@ -2,9 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const eightBall = document.querySelector('.eight-ball'),
         triangles = document.querySelector('.triangle-wrap'),
+        shadow = document.querySelector('.core-shadow'),
         fortune = document.querySelector('.fortune');
 
-  // triangles.style.display = 'none';
+  triangles.style.opacity = '0';
+
+  let opacity = 0;
 
   const answers = [
     'It is certain',
@@ -29,12 +32,53 @@ document.addEventListener('DOMContentLoaded', () => {
     'Very doubtful'
   ]; 
 
+  const shake = () => {
+    setTimeout(() => {
+      eightBall.style.transform = 'translateY(-40px)';
+      shadow.style.transform = 'scale(0.85)';
+    }, 100);
+    setTimeout(() => {
+      eightBall.style.transform = 'translateX(-40px) translateY(-40px)';
+    }, 400);
+    setTimeout(() => {
+      eightBall.style.transform = 'translateX(0) translateY(-40px)';
+    }, 500);
+    setTimeout(() => {
+      eightBall.style.transform = 'translateX(40px) translateY(-40px)';
+    }, 600);
+    setTimeout(() => {
+      eightBall.style.transform = 'translateX(0) translateY(-40px)';
+    }, 700);
+    setTimeout(() => {
+      eightBall.style.transform = 'translateY(0)';
+      shadow.style.transform = 'scale(1)';
+    }, 1000);
+  }
+
+  const fadeIn = () => {
+    if (opacity < 1) {
+      opacity += .1;
+      setTimeout(() => fadeIn(), 100);
+    }
+    triangles.style.opacity = opacity;
+  }
+
+  const fadeOut = () => {
+    if (opacity > 0) {
+      opacity -= .1;
+      setTimeout(() => fadeOut(), 100);
+    }
+    triangles.style.opacity = opacity;
+  }
+
   const askMagicEightBall = () => {
     const random = Math.floor(Math.random() * answers.length);
     fortune.textContent = answers[random];
-    triangles.style.display = 'block';
+    shake();
+    setTimeout(() => fadeIn(), 1000);
+    setTimeout(() => fadeOut(), 4000);
   }
 
-  eightBall.addEventListener('click', () => askMagicEightBall());
+  eightBall.addEventListener('click', () => opacity > 0 ? false : askMagicEightBall() );
 
 });
