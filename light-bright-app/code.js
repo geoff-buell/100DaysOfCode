8ed189ccf4cbc134f6b04130b1a382abb272dcb9
Geoffrey Buell <info@geoffreybuell.com>
2021-04-15 10:12:07 -0400

    Set up basics for the light bright app

---
+++ light-bright-app/code.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const surfaceArea = document.getElementById('surface-area');
+
+  const circle = document.createElement('div');
+  circle.classList.add('circle');
+  surfaceArea.appendChild(circle);
+
+});
