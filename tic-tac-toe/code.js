318594dc8a2eca6a327890651397a7f12d2c2fc7
Geoffrey Buell <info@geoffreybuell.com>
2021-04-16 11:50:21 -0400

    Changed handleClick fn so that player cannot change a tile once it's been assigned x or o

--- tic-tac-toe/code.js
+++ tic-tac-toe/code.js
@@ -21,21 +21,25 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   const handleClick = (tile) => {
-    if (isXTurn) {
+    if (isXTurn && tile.textContent === '') {
       tile.textContent = 'X';
       tilesArr[tile.id] = 'X';
-    } else {
+      isXTurn = !isXTurn;
+      isXTurn ? playerTurn.textContent = 'X' : playerTurn.textContent = 'O';
+    } else if (!isXTurn && tile.textContent === '') {
       tile.textContent = 'O';
       tilesArr[tile.id] = 'O';
+      isXTurn = !isXTurn;
+      isXTurn ? playerTurn.textContent = 'X' : playerTurn.textContent = 'O';
     }
-    isXTurn = !isXTurn;
-    isXTurn ? playerTurn.textContent = 'X' : playerTurn.textContent = 'O';
     checkForWin();
   }
 
   const checkForWin = () => {
     // winning combos
-    // [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+    // [0,1,2], [3,4,5], [6,7,8], [0,3,6], 
+    // [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+    
     tilesArr[0] === tilesArr[1] &&
     tilesArr[1] === tilesArr[2] &&
     tilesArr[0] === tilesArr[2] ?
