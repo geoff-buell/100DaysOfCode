44efd9c7c256326e7399929e5be9b730e69d186a
Geoffrey Buell <info@geoffreybuell.com>
2021-04-19 09:22:49 -0400

    Added startPos group for hover states

--- connect-4/code.js
+++ connect-4/code.js
@@ -1,6 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const gameboard = document.getElementById('gameboard');
+  const startPosWrap = document.getElementById('start-pos-wrap');
+
+  const startPosArr = ['c1','c2','c3','c4','c5','c6','c7'];
+  const addStartPos = () => {
+    startPosArr.forEach((index) => {
+      const startPos = document.createElement('div');
+      const circle = document.createElement('div');
+      circle.classList.add('circle');
+      // circle.style.border = 'none';
+      startPos.id = index;
+      startPos.classList.add('slot');
+      startPos.style.border = 'none';
+      startPos.appendChild(circle);
+      startPosWrap.appendChild(startPos);
+    });
+  }
+
+  addStartPos();
 
   const slotsArr = [
     01, 02, 03, 04, 05, 06, 07,
@@ -20,6 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
       circle.classList.add('circle');
       slot.id = index;
       slot.classList.add('slot');
+      slot.addEventListener('mouseover', () => handleMouseover(slot));
       slot.addEventListener('click', () => dropChip(slot, circle));
       slot.appendChild(circle);
       gameboard.appendChild(slot);
@@ -28,6 +47,21 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   addSlots();
+
+  // [
+  //   01, 02, 03, 04, 05, 06, 07,
+  //   08, 09, 10, 11, 12, 13, 14,
+  //   15, 16, 17, 18, 19, 20, 21,
+  //   22, 23, 24, 25, 26, 27, 28,
+  //   29, 30, 31, 32, 33, 34, 35,
+  //   36, 37, 38, 39, 40, 41, 42
+  // ];
+
+  // const handleMouseover = (slot) => {
+  //   if (slot.id === 01) {
+  //     console.log('hovered');
+  //   }
+  // }
 
   const dropChip = (slot, circle) => {
     console.log(slot);
