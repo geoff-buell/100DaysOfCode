89cd4250801d7b380cd6adccc5274880a77643f0
Geoffrey Buell <info@geoffreybuell.com>
2021-04-28 09:51:03 -0400

    Wrote async fn to fetch cycling data

--- d3-scatter-plot/code.js
+++ d3-scatter-plot/code.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const width = 800;
-  const height = 450;
-  const padding = 40;
+  async function getCyclingData() {
+    const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 
-  const svg = d3.select('#scatter-plot')
-                .append('svg')
-                .attr('width', width)
-                .attr('height', height);
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log(data);
+
+      const width = 800;
+      const height = 450;
+      const padding = 40;
+    
+      const svg = d3.select('#scatter-plot')
+                    .append('svg')
+                    .attr('width', width)
+                    .attr('height', height);
+
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  getCyclingData();
 
 });
