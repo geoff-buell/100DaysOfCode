d4f9bc2eb82384118e5e5725d69a44133bf6ea05
Geoffrey Buell <info@geoffreybuell.com>
2021-04-28 09:41:34 -0400

    Set up basics for scatter plot

---
+++ d3-scatter-plot/code.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const width = 800;
+  const height = 450;
+  const padding = 40;
+
+  const svg = d3.select('#scatter-plot')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height);
+
+});
