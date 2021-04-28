d95f00e8cffb7158ac33a381729f99277ad488ea
Geoffrey Buell <info@geoffreybuell.com>
2021-04-28 11:27:16 -0400

    Added xScale, yScale, xAxis, yAxis, appended axes to scatter plot

--- d3-scatter-plot/code.js
+++ d3-scatter-plot/code.js
@@ -17,6 +17,41 @@ document.addEventListener('DOMContentLoaded', () => {
                     .attr('width', width)
                     .attr('height', height);
 
+      const timeParse = d3.timeParse('%M:%S');              
+      const timeFormat = d3.timeFormat('%M:%S');              
+      
+      const fastestTime = d3.min(data, (d) => timeParse(d.Time));
+      const slowestTime = d3.max(data, (d) => timeParse(d.Time));
+
+      const beginningYear = d3.min(data, (d) => d.Year - 1); 
+      const latestYear = d3.max(data, (d) => d.Year + 1);
+
+      const xScale = d3.scaleLinear()
+                       .domain([beginningYear, latestYear])
+                       .range([padding, width - padding]);
+
+      const xAxis = d3.axisBottom()
+                      .tickFormat(d3.format('d'))
+                      .scale(xScale);
+
+      const yScale = d3.scaleLinear()
+                       .domain([slowestTime, fastestTime])
+                       .range([height - padding, padding]);
+
+      const yAxis = d3.axisLeft()
+                      .tickFormat(timeFormat)
+                      .scale(yScale);
+
+      svg.append('g')
+         .attr('transform', 'translate(0, ' + (height - padding) + ')')
+         .attr('id', 'x-axis')
+         .call(xAxis);
+
+      svg.append('g')
+         .attr('transform', 'translate(' + padding + ', 0)')
+         .attr('id', 'y-axis')
+         .call(yAxis);   
+
     } catch(error) {
       console.log(error);
     }
