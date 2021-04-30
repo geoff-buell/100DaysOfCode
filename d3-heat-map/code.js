84f3df8627b62056279c751f5f3fbdb94c524d77
Geoffrey Buell <info@geoffreybuell.com>
2021-04-30 15:31:44 -0400

    Was able to append data to heat map

--- d3-heat-map/code.js
+++ d3-heat-map/code.js
@@ -7,52 +7,48 @@ document.addEventListener('DOMContentLoaded', () => {
       let response = await fetch(url);
       let data = await response.json();
 
-      console.log(data);
-      console.log(data.baseTemperature);
       console.log(data.monthlyVariance);
-      console.log(data.monthlyVariance[0]);
 
       const width = 1250;
       const height = 500;
       const padding = 80;
+      const rectWidth = (width - 2 * padding) / 262;
+      const rectHeight = (height - 2 * padding) / 12;
 
       const heatmap = d3.select('#heat-map')
                         .append('svg')
                         .attr('width', width)
                         .attr('height', height);
 
-      const months = [
-        'January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December'
+      const colors = [
+        '#0091ad', '#1780a1', '#2e6f95', '#455e89', '#5c4d7d',
+        '#723c70', '#892b64', '#a01a58', '#b7094c'
       ];                  
 
       const baseTemp = data.baseTemperature;
 
-      const minYear = d3.min(data.monthlyVariance, (d) => d.year);
-      const maxYear = d3.max(data.monthlyVariance, (d) => d.year);
+      console.log(d3.min(data.monthlyVariance, (d) => d.variance));
+      console.log(d3.max(data.monthlyVariance, (d) => d.variance));
       
       const xScale = d3.scaleTime()
-                       .domain([minYear, maxYear])
-                       .range([padding, width - padding])
-                       .nice();       
+                       .domain(d3.extent(data.monthlyVariance, (d) => d.year))
+                       .range([padding, width - padding]);  
+                       
+      const yScale = d3.scaleTime()
+                       .domain(d3.extent(data.monthlyVariance, (d) => d.month))
+                       .range([padding, height - padding]);
       
-      const yScale = d3.scaleBand()
-                       .domain([1,2,3,4,5,6,7,8,9,10,11,12])
-                       .rangeRound([padding - 50, height - padding]);
+      const customYScale = d3.scaleTime()
+                       .domain([new Date(2021, 11, 31), new Date(2021, 0, 1)])
+                       .range([height - padding, padding]);
 
       const xAxis = d3.axisBottom()
                       .tickFormat(d3.format('d'))
-                      .scale(xScale);
-
+                      .scale(xScale);       
+      
       const yAxis = d3.axisLeft()
-                      .tickValues(yScale.domain())
-                      .tickFormat((month) => {
-                        const date = new Date(0);
-                        const timeFormat = d3.timeFormat('%B');
-                        date.setUTCMonth(month);
-                        return timeFormat(date);
-                      })
-                      .scale(yScale);                
+                      .tickFormat(d3.timeFormat('%B'))
+                      .scale(customYScale);
                       
       heatmap.append('g')
              .attr('transform', 'translate(0, ' + (height - padding) + ')')
@@ -63,6 +59,21 @@ document.addEventListener('DOMContentLoaded', () => {
              .attr('transform', 'translate(' + padding + ', 0)')
              .attr('id', 'y-axis')
              .call(yAxis);  
+
+      heatmap.selectAll('.cell')
+             .data(data.monthlyVariance)
+             .enter()
+             .append('rect')
+             .attr('class', 'cell')
+             .attr('data-month', (d) => d.month)
+             .attr('data-year', (d) => d.year)
+             .attr('data-temp', (d) => baseTemp + d.variance)
+             .attr('x', (d) => xScale(d.year))
+             .attr('y', (d) => yScale(d.month)) 
+             .attr('width', rectWidth)
+             .attr('height', rectHeight)       
+             .attr("transform", "translate(0, -29)") 
+             .attr('fill', '#2e6f95');           
 
     } catch(error) {
       console.log(error);
