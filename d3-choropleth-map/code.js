03b8910ec33a97d00572492a7893cf576b7b4e99
Geoffrey Buell <info@geoffreybuell.com>
2021-05-03 17:06:44 -0400

    Added color to map

--- d3-choropleth-map/code.js
+++ d3-choropleth-map/code.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const width = 900;
+  const width = 950;
   const height = 600;
 
   const map = d3.select('#map')
@@ -8,13 +8,32 @@ document.addEventListener('DOMContentLoaded', () => {
                 .attr('width', width)
                 .attr('height', height);
 
+  const colors = [
+    '#427EF5', '#3B70DA', '#3463C0', '#2D55A5', '#25478A', '#1E396F', '#172C55', '#101E3A'
+  ];        
+
   const drawMap = () => {
     map.selectAll('path')
        .data(countyData)
        .enter()
        .append('path')
        .attr('d', d3.geoPath())
-       .attr('class', 'county');
+       .attr('class', 'county')
+       .attr('fill', (countyDataItem) => {
+         const id = countyDataItem['id'];
+         const county = educationData.find((item) => {
+           return item['fips'] === id;
+         });
+         const percentage = county['bachelorsOrHigher'];
+         if (percentage >= 0 && percentage < 10) { return colors[0] }
+         else if (percentage >= 10 && percentage < 20) { return colors[1] }
+         else if (percentage >= 20 && percentage < 30) { return colors[2] }
+         else if (percentage >= 30 && percentage < 40) { return colors[3] }
+         else if (percentage >= 40 && percentage < 50) { return colors[4] }
+         else if (percentage >= 50 && percentage < 60) { return colors[5] }
+         else if (percentage >= 60 && percentage < 70) { return colors[6] }
+         else if (percentage >= 70 && percentage < 80) { return colors[7] } 
+       });
   }
 
   const countyURL = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json';
@@ -47,5 +66,5 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   );
-  
+
 }); 
