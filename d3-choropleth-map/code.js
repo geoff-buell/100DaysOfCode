f2a47033894fb106d44f6add4574991887a021f8
Geoffrey Buell <info@geoffreybuell.com>
2021-05-04 09:43:46 -0400

    Added legend and styling

--- d3-choropleth-map/code.js
+++ d3-choropleth-map/code.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const colors = [
+    '#427EF5', '#3B70DA', '#3463C0', '#2D55A5', '#25478A', '#1E396F', '#172C55', '#101E3A'
+  ];  
+
+  const colorData = (percentage) => {
+    if (percentage >= 0 && percentage < 10) { return colors[0] }
+    else if (percentage >= 10 && percentage < 20) { return colors[1] }
+    else if (percentage >= 20 && percentage < 30) { return colors[2] }
+    else if (percentage >= 30 && percentage < 40) { return colors[3] }
+    else if (percentage >= 40 && percentage < 50) { return colors[4] }
+    else if (percentage >= 50 && percentage < 60) { return colors[5] }
+    else if (percentage >= 60 && percentage < 70) { return colors[6] }
+    else if (percentage >= 70 && percentage < 80) { return colors[7] }
+  }
+
   const width = 950;
   const height = 600;
 
@@ -8,9 +23,34 @@ document.addEventListener('DOMContentLoaded', () => {
                 .attr('width', width)
                 .attr('height', height);
 
-  const colors = [
-    '#427EF5', '#3B70DA', '#3463C0', '#2D55A5', '#25478A', '#1E396F', '#172C55', '#101E3A'
-  ];        
+  const legendData = [0, 10, 20, 30, 40, 50, 60, 70]; 
+  const legendCellWidth = 40;             
+
+  const legend = map.append('svg')
+                    .attr('x', width - (80 + legendCellWidth * legendData.length))
+                    .attr('y', 20)
+                    .attr('width', legendCellWidth * legendData.length)
+                    .attr('height', 100)
+                    .attr('id', 'legend');  
+                    
+  legend.selectAll('.legend')
+        .data(legendData)
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => (legendCellWidth * i))
+        .attr('y', 0)
+        .attr('width', legendCellWidth)
+        .attr('height', 10)
+        .style('fill', (d) => colorData(d));   
+        
+  legend.selectAll('.text')
+        .data(legendData)
+        .enter()
+        .append('text')
+        .text((d) => d.toString() + '%')
+        .attr('x', (d, i) => legendCellWidth * i)
+        .attr('y', 25)
+        .style('font-size', 12 + 'px')
 
   const drawMap = () => {
     map.selectAll('path')
@@ -49,18 +89,17 @@ document.addEventListener('DOMContentLoaded', () => {
       if (error) {
         console.log(error); 
       } else {
-        countyData = topojson.feature(data, data.objects.counties).features;
-        console.log(countyData);
-
+        countyData = topojson.feature(data, data.objects.counties).features;  
+        // console.log(countyData);
         d3.json(educationURL).then(
           (data, error) => {
             if (error) {
               console.log(error); 
             } else {
               educationData = data;
-              console.log(data);
-              console.log(d3.min(data, (d) => d.bachelorsOrHigher));
-              console.log(d3.max(data, (d) => d.bachelorsOrHigher));
+              // console.log(data);
+              // console.log(d3.min(data, (d) => d.bachelorsOrHigher));
+              // console.log(d3.max(data, (d) => d.bachelorsOrHigher));
               drawMap();
             }
           }
