49e7915656fbf83ee915979f4d1f224b70a8897b
Geoffrey Buell <info@geoffreybuell.com>
2021-05-04 16:52:23 -0400

    Established width and height and appended svg

--- d3-tree-map/code.js
+++ d3-tree-map/code.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const width = 1000;
+  const height = 600;
+
+  const treeMap = d3.select('#tree-map')
+                    .append('svg')
+                    .attr('width', width)
+                    .attr('height', height);
+
   const url = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json';
 
   let gameData;
@@ -11,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
       } else {
         gameData = data;
         console.log(gameData);
+        console.log(gameData.children);
       }
     }
   )
