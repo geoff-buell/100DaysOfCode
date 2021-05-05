d230d466770d66b4559158c1cdff871bfd46fb07
Geoffrey Buell <info@geoffreybuell.com>
2021-05-05 15:26:12 -0400

    Established hierarchy, gameTiles, appended g and rect for gameTiles

--- d3-tree-map/code.js
+++ d3-tree-map/code.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const colors = [
+    '#F8997D', '#F4937C', '#EF8D7B', '#EB877A', '#E68179', '#E27B78',
+    '#DE7577', '#D96F76', '#D56975', '#D06375', '#CC5D74', '#C75773', 
+    '#C35172', '#BF4B71', '#BA4570', '#B63F6F', '#B1396E', '#AD336D'
+  ];
+
   const width = 1000;
   const height = 600;
 
@@ -7,6 +13,88 @@ document.addEventListener('DOMContentLoaded', () => {
                     .append('svg')
                     .attr('width', width)
                     .attr('height', height);
+
+  const drawTreeMap = () => {
+    const hierarchy = d3.hierarchy(gameData, (d) => d.children)
+                        .sum((d) => d.value)
+                        .sort((a, b) => b.value - a.value);
+
+    const createTreeMap = d3.treemap()
+                            .size([1000, 600]);
+
+    createTreeMap(hierarchy);
+
+    const gameTiles = hierarchy.leaves();
+    console.log(gameTiles);  
+
+    const section = treeMap.selectAll('g')
+                            .data(gameTiles)
+                            .enter()
+                            .append('g');
+
+    section.append('rect')
+            .attr('class', 'tile')
+            .attr('fill', (d) => {
+              const category = d.data.category;
+              let color;
+              switch (category) {
+                case 'Wii':
+                  color = colors[0];
+                  break;
+                case 'DS':
+                  color = colors[1];
+                  break;
+                case 'X360':
+                  color = colors[2];
+                  break;
+                case 'GB':
+                  color = colors[3];
+                  break;
+                case 'PS3':
+                  color = colors[4];
+                  break;
+                case 'NES':
+                  color = colors[5];
+                  break;
+                case 'PS2':
+                  color = colors[6];
+                  break;
+                case '3DS':
+                  color = colors[7];
+                  break;
+                case 'PS4':
+                  color = colors[8];
+                  break;
+                case 'SNES':
+                  color = colors[9];
+                  break;
+                case 'PS':
+                  color = colors[10];
+                  break;
+                case 'N64':
+                  color = colors[11];
+                  break;
+                case 'GBA':
+                  color = colors[12];
+                  break;
+                case 'XB':
+                  color = colors[13];
+                  break;
+                case 'PC':
+                  color = colors[14];
+                  break;
+                case '2600':
+                  color = colors[15];
+                  break;
+                case 'PSP':
+                  color = colors[16];
+                  break;
+                case 'XOne':
+                  color = colors[17];    
+              }
+              return color;
+            })
+  }
 
   const url = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json';
 
@@ -18,8 +106,8 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(error);
       } else {
         gameData = data;
-        console.log(gameData);
-        console.log(gameData.children);
+        // console.log(gameData);
+        drawTreeMap();
       }
     }
   )
