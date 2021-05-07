1b129b4d31a0bfc3081c6f5325f7e4f41e82823c
Geoffrey Buell <info@geoffreybuell.com>
2021-05-07 11:04:58 -0400

    Set up files for flipbox art

---
+++ flipbox-art/code.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const container = document.querySelector('.container');
+
+  const flipBox = document.createElement('div');
+  const flipBoxInner = document.createElement('div');
+  const flipBoxFront = document.createElement('div');
+  const flipBoxBack = document.createElement('div');
+
+});
