80d4e997891d7c92cfc648ce91d4a68824bdfd96
Geoffrey Buell <info@geoffreybuell.com>
2021-05-07 11:57:42 -0400

    Appended 48 flipboxes in js

--- flipbox-art/code.js
+++ flipbox-art/code.js
@@ -2,9 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const container = document.querySelector('.container');
 
-  const flipBox = document.createElement('div');
-  const flipBoxInner = document.createElement('div');
-  const flipBoxFront = document.createElement('div');
-  const flipBoxBack = document.createElement('div');
+  const flipboxArr = [
+    'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
+    'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
+    'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
+    'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
+    'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f',
+    'f', 'f', 'f', 'f', 'f', 'f', 'f', 'f'
+  ]
+
+  flipboxArr.forEach(() => {
+    const flipBox = document.createElement('div');
+    const flipBoxInner = document.createElement('div');
+    const flipBoxFront = document.createElement('div');
+    const flipBoxBack = document.createElement('div');
+    flipBox.classList.add('flip-box');
+    flipBoxInner.classList.add('flip-box-inner');
+    flipBoxFront.classList.add('flip-box-front');
+    flipBoxBack.classList.add('flip-box-back');
+    flipBoxInner.appendChild(flipBoxFront);
+    flipBoxInner.appendChild(flipBoxBack);
+    flipBox.appendChild(flipBoxInner);
+    container.appendChild(flipBox);
+  });
+
 
 });
