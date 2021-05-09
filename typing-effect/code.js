1af68b52a40a35f08fc9861612ae774277e1b6b6
Geoffrey Buell <info@geoffreybuell.com>
2021-05-08 23:37:15 -0400

    update

--- typing-effect/code.js
+++ typing-effect/code.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   const container = document.querySelector('.container');
+  const content = document.createElement('p');
 
+  container.appendChild(content);
+
+  const text = 'Hello, my name is Geoffrey Buell.';
+  let i = 0;
+
+  const typeContent = () => {
+    if (i < text.length) {
+      content.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeContent, 150);
+    }
+  }
   
+  typeContent();
 
 });
