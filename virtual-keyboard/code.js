9d7feaaeac201f215893287baa8b16c71b6c4147
Geoffrey Buell <info@geoffreybuell.com>
2021-06-16 09:31:20 -0400

    Added functionality to space-bar

--- virtual-keyboard/code.js
+++ virtual-keyboard/code.js
@@ -7,11 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
   keys.forEach((key) => key.addEventListener('click', () => {
 
     if (key.id === 'delete') {
-      text.value = text['value'].substring(0, text['value'].length - 1);
+      text['value'] = text['value'].substring(0, text['value'].length - 1);
     } else if (key.id === 'return') {
-      text.value = text.value + '\n';
+      text['value'] = text.value + '\n';
+    } else if (key.id === 'space-bar') {
+      text['value'] = text.value + ' ';
     } else {
-      text.value = text.value + key.id;
+      text['value'] = text.value + key.id;
     }
 
     
