611db6d9b07320332ef13e9bc53a8ce0ea807f86
Geoffrey Buell <info@geoffreybuell.com>
2021-06-16 09:51:51 -0400

    Started putting specific key handles in their own functions to clean up code

--- virtual-keyboard/code.js
+++ virtual-keyboard/code.js
@@ -3,17 +3,35 @@ document.addEventListener('DOMContentLoaded', () => {
   const capsLight = document.getElementById('caps-light');
   const text = document.getElementById('text');
   const keys = document.querySelectorAll('.key');
+
+  function handleDelete() {
+    text.value = text['value'].substring(0, text['value'].length - 1);
+    //need to add ability to delete selected text
+  }
+
+  function handleReturn() {
+    text.value = text.value + '\n';
+  }
+
+  function handleSpaceBar() {
+    text.value = text.value + ' ';
+  }
+
+  function handleTab() {
+    text.value = text.value + '        ';
+  }
   
   keys.forEach((key) => key.addEventListener('click', () => {
 
-    if (key.id === 'delete') {
-      text['value'] = text['value'].substring(0, text['value'].length - 1);
+    if (key.id === 'delete') { handleDelete();
     } else if (key.id === 'return') {
-      text['value'] = text.value + '\n';
+      handleReturn();
     } else if (key.id === 'space-bar') {
-      text['value'] = text.value + ' ';
+      handleSpaceBar();
+    } else if (key.id === 'tab') {
+      handleTab();
     } else {
-      text['value'] = text.value + key.id;
+      text.value = text.value + key.id;
     }
 
     
